7fc41971a29d56de21f863be1bb83c6babd03623
Edwardo <ejace1@gmail.com>
2019-04-05 11:40:33 +0000

    Create messages table

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,10 @@ User.create(username: "andre", password: "password")
 User.create(username: "brienne", password: "password")
 User.create(username: "tyrion", password: "password")
 User.create(username: "dracarys", password: "password")
+
+
+Message.create(body: "Hi, I'm a new message", user: User.last)
+Message.create(body: "Hi, I'm a second message", user: User.last)
+Message.create(body: "Dracarys!", user: User.first)
+Message.create(body: "This enemy does not stop. Does not tire.", user: User.first)
+Message.create(body: "Does not feel.", user: User.last)
