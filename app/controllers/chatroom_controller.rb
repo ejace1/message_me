c1c4460473679892ec7f37a32c8ce5cd1365fd78
Edwardo <ejace1@gmail.com>
2019-04-05 22:20:05 +0000

    Add messages from UI

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,11 +1,14 @@
 class ChatroomController < ApplicationController
+  before_action :require_user
   
   def index
+    @message = Message.new
     @messages = Message.all
   end
   
   def new
     
   end
+
   
 end
